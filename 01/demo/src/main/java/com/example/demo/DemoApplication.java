c4759028d159815f89c4354c1ddc84632b317f1c
wangguan111 <wang_guan111@126.com>
2018-07-02 23:38:04 +0800

    spring-boot demo

---
+++ 01/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//http://localhost:8080/hello
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
